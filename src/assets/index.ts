d6b6b08cd63a31114f7eb85099746a365c3bf02a
Kangonga <guchu413@gmail.com>
2023-06-14 15:14:43 +0300

    feat: add assets and a way to expose them to the app via index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,9 @@
-export const images = {};
+import trianglesBG from '@app/assets/images/triangles.svg';
+import spinner from '@app/assets/images/Spinner-1s-200px.svg';
+import NotFound from '@app/assets/images/404.png';
+
+export const images = {
+  trianglesBG,
+  spinner,
+  NotFound,
+};
