02b56268dfc11d04e24de81db923d005cc62d6b2
Kangonga <guchu413@gmail.com>
2023-06-14 15:20:54 +0300

    feat: include routing at main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "@app/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider } from 'react-router-dom';
+import router from '@app/routes';
+import { queryClient } from '@app/context/queryContext';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
