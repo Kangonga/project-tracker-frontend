c7f621d7620c64e995343758c3726f1ccdc2354c
Kangonga <guchu413@gmail.com>
2023-06-14 15:21:19 +0300

    feat: handle checking auth status and subsequent routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
+import { images } from '@app/assets';
+import { AuthContext } from '@app/context/authContext';
+import { getAuthStatus } from '@app/features/authentication/hooks/getAuthStatus';
+import { CssBaseline } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
 export default function App() {
-  return <></>;
+  const [isAuth, setIsAuth] = useState(false);
+  const { data, isLoading, isFetching, isError, isFetched } = useQuery({
+    queryKey: ['auth'],
+    queryFn: getAuthStatus,
+  });
+  return (
+    <AuthContext.Provider value={{ isAuth, setIsAuth }}>
+      {isError && <img style={{ width: '100dvw', height: '100dvh' }} src={images.NotFound} alt="error image" />}
+      {isLoading && isFetching && (
+        <img style={{ width: '100dvw', height: '100dvh' }} src={images.spinner} alt="loading screen spinner" />
+      )}
+      <>{isFetched && data.status === 404 && <Navigate to="auth" />}</>
+      <CssBaseline />
+      <Outlet />
+    </AuthContext.Provider>
+  );
 }
