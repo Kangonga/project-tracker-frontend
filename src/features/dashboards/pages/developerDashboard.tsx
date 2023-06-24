d5f418419e5cbe536c21b045026fc4d49e21e042
Kangonga <guchu413@gmail.com>
2023-06-24 15:04:09 +0300

    feat: display the kanban board in the developer dashboard

--- src/features/dashboards/pages/developerDashboard.tsx
+++ src/features/dashboards/pages/developerDashboard.tsx
@@ -1,7 +1,10 @@
+import Kanban from '@app/features/kanbanBoards/components/board/kanbanBoard';
+
 export default function DeveloperDashboard() {
   return (
     <>
-      <h1>Hello developer</h1>
+      <h1>Developer Dashboard</h1>
+      <Kanban />
     </>
   );
 }
