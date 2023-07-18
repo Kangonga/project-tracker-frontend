91857a5ec2e8f14ccb8511826a75aac8e5d3ec1f
Kangonga <guchu413@gmail.com>
2023-07-18 09:25:21 +0300

    feat: create component container

---
+++ src/layouts/pageContainer/pageContainer.tsx
@@ -0,0 +1,9 @@
+import { styled } from '@mui/material';
+
+export const PageContainer = styled('div')(({}) => ({
+  display: 'flex',
+  flex: '3',
+  flexDirection: 'column',
+  minHeight: '100vh',
+  backgroundColor: '#EBDFD7',
+}));
