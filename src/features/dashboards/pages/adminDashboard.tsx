a86fbfd1dabbbafdfeae8a41f466e9e0ef236840
Kangonga <guchu413@gmail.com>
2023-07-18 09:26:12 +0300

    feat: render the tobar in the dashboard

--- src/features/dashboards/pages/adminDashboard.tsx
+++ src/features/dashboards/pages/adminDashboard.tsx
@@ -1,9 +1,16 @@
+import { PageContainer } from '@app/layouts/pageContainer/pageContainer';
 import Sidebar from '@app/layouts/sidebar/sidebar';
+import Topbar from '@app/layouts/topbar/topbar';
+import { Divider } from '@mui/material';
 
 export default function AdminDashboard() {
   return (
     <div style={{ display: 'flex' }}>
       <Sidebar />
+      <PageContainer>
+        <Topbar />
+        <Divider />
+      </PageContainer>
     </div>
   );
 }
