2a7e2f06de7235eb30c313e5da5d97d91113b577
Kangonga <guchu413@gmail.com>
2023-07-18 12:24:20 +0300

    feat: render the dashboard cards

--- src/features/dashboards/pages/adminDashboard.tsx
+++ src/features/dashboards/pages/adminDashboard.tsx
@@ -1,6 +1,9 @@
+import { CardContainer } from '@app/features/dashboards/components/dashboardCard/cardContainer';
+import DashboardCard from '@app/features/dashboards/components/dashboardCard/dashboardCard';
 import { PageContainer } from '@app/layouts/pageContainer/pageContainer';
 import Sidebar from '@app/layouts/sidebar/sidebar';
 import Topbar from '@app/layouts/topbar/topbar';
+import { BallotOutlined, BugReportOutlined, EngineeringOutlined, FeaturedPlayListOutlined } from '@mui/icons-material';
 import { Divider } from '@mui/material';
 
 export default function AdminDashboard() {
@@ -10,6 +13,12 @@ export default function AdminDashboard() {
       <PageContainer>
         <Topbar />
         <Divider />
+        <CardContainer>
+          <DashboardCard smallText="Projects" bigText="5" icon={<BallotOutlined />} />
+          <DashboardCard smallText="Developers" bigText="3" icon={<EngineeringOutlined />} />
+          <DashboardCard smallText="Issues" bigText="10" icon={<BugReportOutlined />} />
+          <DashboardCard smallText="Features in progress" bigText="5" icon={<FeaturedPlayListOutlined />} />
+        </CardContainer>
       </PageContainer>
     </div>
   );
