3c77afcea9c0e16f93656f2beff2dc6f9ca9dc9a
Kangonga <guchu413@gmail.com>
2023-07-18 09:23:48 +0300

    style: remove background color

--- src/layouts/topbar/components/topbarContainer.tsx
+++ src/layouts/topbar/components/topbarContainer.tsx
@@ -3,7 +3,6 @@ import { styled } from '@mui/material';
 export const TopbarContainer = styled('div')(({}) => ({
   display: 'flex',
   justifyContent: 'space-between',
-  backgroundColor: 'black',
   minHeight: '10vh',
   alignItems: 'center',
   padding: '1rem',
